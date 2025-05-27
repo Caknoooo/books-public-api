90fc1ed35ed0148f2cf76ca2f9c55ce3ff189f98
Caknoooo <caknocomel@gmail.com>
2025-05-27 09:21:12 +0700

    fix: error something

--- routes/book_route.go
+++ routes/book_route.go
@@ -3,9 +3,12 @@ package routes
 import (
 	"github.com/Caknoooo/go-gin-clean-starter/controller"
 	"github.com/gin-gonic/gin"
+	"github.com/samber/do"
 )
 
-func Books(route *gin.Engine, booksController controller.BooksController) {
+func Books(route *gin.Engine, injector *do.Injector) {
+	booksController := do.MustInvoke[controller.BooksController](injector)
+
 	books := route.Group("/api/books")
 	{
 		books.GET("/", booksController.GetAllBooks)
